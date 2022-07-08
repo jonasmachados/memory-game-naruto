f11aa11acbfba19a3960448309af5695bf3320a5
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-08 17:53:11 -0300

    created function to validation cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+    
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 800);
 }
 
 cards.forEach((card) => {
