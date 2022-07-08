8c4d5d66789d4aed437f5759bb3580fb87f9f3a7
jonasmachados <jonasmachado.ti@gmail.com>
2022-07-08 18:16:54 -0300

    Leaving the card in random order

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ let lockBoard = false;
 
 function flipCard(){
     if(lockBoard) return;
-    
+    if(this === firstCard) return;
+
     this.classList.add('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -30,14 +31,31 @@ function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
     
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 800);
 }
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
